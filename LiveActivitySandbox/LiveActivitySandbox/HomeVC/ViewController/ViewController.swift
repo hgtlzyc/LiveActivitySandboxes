71ec48efd55734d536f9fe6bcf01ae16578a130b
Lijia Xu <hgtlzycwk@gmail.com>
2023-03-29 12:22:13 -0400

    setup initial files and scructures

---
+++ LiveActivitySandbox/LiveActivitySandbox/HomeVC/ViewController/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LiveActivitySandbox
+//
+//  Created by lijia xu on 3/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
+// MARK: - Setup Helpers
+private extension ViewController {
+    
+}
+
