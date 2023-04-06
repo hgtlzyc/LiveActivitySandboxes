7ddac135453c14413921a70add2542dd065e003e
Lijia Xu <hgtlzycwk@gmail.com>
2023-04-05 20:53:05 -0400

    update logics end the live activity

--- LiveActivitySandbox/LiveActivitySandbox/Support/AppDelegate.swift
+++ LiveActivitySandbox/LiveActivitySandbox/Support/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ActivityKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -24,11 +25,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        Task { @MainActor in
+            await endAllLiveActivities()
+        }
     }
+}
 
-
+private extension AppDelegate {
+    func endAllLiveActivities() async {
+        let finalState = WorkoutLiveActivityAttributes.ContentState.empthState
+        let finalContent = ActivityContent(
+            state: finalState, staleDate: Date()
+        )
+        for activity in Activity<WorkoutLiveActivityAttributes>.activities {
+            await activity.end(finalContent, dismissalPolicy: .immediate)
+        }
+        
+        Log.info("End Previous Activities")
+    }
 }
 
